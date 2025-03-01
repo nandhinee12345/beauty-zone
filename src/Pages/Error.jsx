bfef964118b47c613e5d7d70a848330625f1c85f
nandhinee12345 <nandhineelb61970@gmail.com>
2025-03-01 11:53:08 +0530

    this is first commit

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 export default function Error() {
-  return <div>Error</div>;
+  return <div>
+    <img src="https://cdn.dribbble.com/userupload/24450589/file/original-7a69eb5b87401ce59325c3291535aebc.gif"/></div>;
 }
